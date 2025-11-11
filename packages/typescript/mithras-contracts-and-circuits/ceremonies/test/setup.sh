89c3e5ae49113c2468e0938267869698799815b1
Joe Polny <joepolny@gmail.com>
2025-11-11 14:09:19 -0500

    chore: setup in zkey.sh

--- packages/typescript/mithras-contracts-and-circuits/ceremonies/test/setup.sh
+++ packages/typescript/mithras-contracts-and-circuits/ceremonies/test/setup.sh
@@ -1,4 +1,6 @@
-snarkjs powersoftau new bls12381 16 pot16_0000.ptau -v
-echo "blah" | snarkjs powersoftau contribute pot16_0000.ptau pot16_0001.ptau --name="First contribution" -v
-snarkjs powersoftau prepare phase2 pot16_0001.ptau pot16_final.ptau -v
-snarkjs powersoftau verify pot16_final.ptau
+if [ ! -f pot16_final.ptau ]; then
+    pnpm snarkjs powersoftau new bls12381 16 pot16_0000.ptau -v
+    echo "blah" | pnpm snarkjs powersoftau contribute pot16_0000.ptau pot16_0001.ptau --name="First contribution" -v
+    pnpm snarkjs powersoftau prepare phase2 pot16_0001.ptau pot16_final.ptau -v
+    pnpm snarkjs powersoftau verify pot16_final.ptau
+fi
--- packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
+++ packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
@@ -1,3 +1,5 @@
+bash ../ceremonies/test/setup.sh
+
 circom --r1cs --wasm --c --sym --inspect deposit.circom --prime bls12381
 pnpm snarkjs plonk setup deposit.r1cs ../ceremonies/test/pot16_final.ptau deposit_test.zkey
 
