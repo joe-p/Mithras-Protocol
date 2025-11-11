cb642940908960184b0f1eaaf814a5442ddf3b69
Joe Polny <joepolny@gmail.com>
2025-11-11 14:02:27 -0500

    ci: download circom

--- packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
+++ packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
@@ -1,5 +1,5 @@
 circom --r1cs --wasm --c --sym --inspect deposit.circom --prime bls12381
-snarkjs plonk setup deposit.r1cs ../ceremonies/test/pot16_final.ptau deposit_test.zkey
+pnpm snarkjs plonk setup deposit.r1cs ../ceremonies/test/pot16_final.ptau deposit_test.zkey
 
 circom --r1cs --wasm --c --sym --inspect spend.circom --prime bls12381
-snarkjs plonk setup spend.r1cs ../ceremonies/test/pot16_final.ptau spend_test.zkey
+pnpm snarkjs plonk setup spend.r1cs ../ceremonies/test/pot16_final.ptau spend_test.zkey
