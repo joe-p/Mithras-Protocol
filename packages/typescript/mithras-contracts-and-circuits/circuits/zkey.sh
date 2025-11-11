7a7a22af09077fc4812f5520a7ba91dec946cc46
Joe Polny <joepolny@gmail.com>
2025-11-11 18:21:17 -0500

    ci: proper paths in zkey.sh

--- packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
+++ packages/typescript/mithras-contracts-and-circuits/circuits/zkey.sh
@@ -1,7 +1,10 @@
+cd $(dirname "$0")
+CIRCUITS_DIR=$(pwd)
+
 bash ../ceremonies/test/setup.sh
 
-circom --r1cs --wasm --c --sym --inspect deposit.circom --prime bls12381
-pnpm snarkjs plonk setup deposit.r1cs ../ceremonies/test/pot16_final.ptau deposit_test.zkey
+circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/deposit.circom --prime bls12381
+pnpm snarkjs plonk setup $CIRCUITS_DIR/deposit.r1cs $CIRCUITS_DIR/../ceremonies/test/pot16_final.ptau $CIRCUITS_DIR/deposit_test.zkey
 
-circom --r1cs --wasm --c --sym --inspect spend.circom --prime bls12381
-pnpm snarkjs plonk setup spend.r1cs ../ceremonies/test/pot16_final.ptau spend_test.zkey
+circom --r1cs --wasm --c --sym --inspect $CIRCUITS_DIR/spend.circom --prime bls12381
+pnpm snarkjs plonk setup $CIRCUITS_DIR/spend.r1cs $CIRCUITS_DIR/../ceremonies/test/pot16_final.ptau $CIRCUITS_DIR/spend_test.zkey
